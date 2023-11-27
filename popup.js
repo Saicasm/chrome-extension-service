c7926189f723ddf43018a6e002d2c2b333dc567a
Sai Kumar N <sai98nsk@gmail.com>
2023-11-27 14:55:56 -0500

    Feat: API Integration

--- popup.js
+++ popup.js
@@ -16,20 +16,44 @@ function captureData() {
   const searchText = "Software"; // Replace with the text you want to find
   console.log("test");
   const wholeDOM = document.documentElement.outerHTML;
-  console.log("DOM", wholeDOM);
-  const elementsWithText = [...document.querySelectorAll("*")].filter(
-    (element) => {
-      return element.textContent.includes(searchText);
-    }
+  const targetElement = document.querySelector(
+    ".scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details"
   );
+  const htmlContent = targetElement.innerHTML;
 
-  if (elementsWithText.length > 0) {
-    const data = elementsWithText[0].textContent;
-    // console.log("dta", data);
-    sendDataToBackend(data);
-  } else {
-    console.log(`No element containing "${searchText}" found.`);
-  }
+  console.log("Roles", targetElement);
+
+  const apiUrl = "http://localhost:8080/api/v1/ingest/saicsm@gmail.com"; // Replace with your API URL
+  //application-outlet
+  // Make a GET request to the Go API
+  fetch(apiUrl, {
+    method: "POST",
+    body: htmlContent,
+    // headers: {
+    //   "Content-type": "application/json; charset=UTF-8",
+    // },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Data from Go API:", data);
+      // You can process the data as needed
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+  // const elementsWithText = [...document.querySelectorAll("*")].filter(
+  //   (element) => {
+  //     return element.textContent.includes(searchText);
+  //   }
+  // );
+
+  // if (elementsWithText.length > 0) {
+  //   const data = elementsWithText[0].textContent;
+  //   // console.log("dta", data);
+  //   sendDataToBackend(data);
+  // } else {
+  //   console.log(`No element containing "${searchText}" found.`);
+  // }
 }
 
 function sendDataToBackend(data) {
